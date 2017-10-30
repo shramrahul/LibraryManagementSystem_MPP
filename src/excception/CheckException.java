f66c957c6010b3abbf0782d8e22cc74883da0482
shramrahul <shreeramchaulagain@gmail.com>
2017-10-30 18:27:47 -0500

    committed..

---
+++ src/excception/CheckException.java
@@ -0,0 +1,17 @@
+package excception;
+
+import java.io.Serializable;
+
+public class CheckException extends Exception implements Serializable {
+    public CheckException() {
+        super();
+    }
+
+    public CheckException(String message) {
+        super(message);
+    }
+
+    public CheckException(Throwable cause) {
+        super(cause);
+    }
+}
